cd4a71f3df8914dcf436e49487cda53f35b7a0fa
Amber Jones <amberlovescats14@gmail.com>
2019-07-19 17:21:55 -0500

    first

---
+++ hosted-resume/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="border"
+        style={{border: '4px solid black'}}>
+       <img src="./img/resume.jpeg" alt="resume"/>
+        
+        </div>
+    </div>
+  );
+}
+
+export default App;
