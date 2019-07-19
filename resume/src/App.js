140dfeb0f4eef6d20ba84f626f0d009df274aa11
Amber Jones <amberlovescats14@gmail.com>
2019-07-19 18:16:03 -0500

    hosted resume

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <div className="wrapper" style={{background: '	#DCDCDC'}} >
+    <img src={require('./img/AJresume.png')} 
+    style={{border:'4px solid black'}}
+    alt="why"/>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
